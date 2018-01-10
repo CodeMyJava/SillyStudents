acf35e42eeda2dbba63de8bbe090bd838ca40b5a
John <Applenourishment@gmail.com>
2018-01-09 16:11:07 -0800

    Setup
    Ensured that the  virtual hosting is working properly for SillyStudents

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <titile></titile>
+    </head>
+    <body>
+       <?php 
+       echo "Hello out there ... I live";
+       ?>
+    </body>
+</html>
+
+
