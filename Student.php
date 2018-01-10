09fb9e3cd059217a424223ac157711646e21d3d2
John <Applenourishment@gmail.com>
2018-01-09 16:29:56 -0800

    Added 2 students
    Added John Doe, and Albert Einstein

--- Student.php
+++ Student.php
@@ -14,27 +14,27 @@
  */
 class Student {
     
-    function __construct() {
-        $this->surname = '';
-        $this->first_name = '';
-        $this->emails = array();
-        $this->grades = array();
-    }
+function __construct() {
+    $this->surname = '';
+    $this->first_name = '';
+    $this->emails = array();
+    $this->grades = array();
+}
     
-    function add_email($which,$address) {
-        $this->emails[$which] = $address;
-    }
+function add_email($which,$address) {
+    $this->emails[$which] = $address;
+}
 
-    function add_grade($grade) {
-        $this->grades[] = $grade;
-    }
+function add_grade($grade) {
+    $this->grades[] = $grade;
+}
     
-    function average() {
-        $total = 0;
-        foreach ($this->grades as $value)
-            $total += $value;
-        return $total / count($this->grades);
-    }
+function average() {
+    $total = 0;
+    foreach ($this->grades as $value)
+        $total += $value;
+    return $total / count($this->grades);
+}
     
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
